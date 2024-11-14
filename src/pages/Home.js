619a18761e9fde5c0ffbd1903243bf18fcd107a5
MateoMina <mateominaclavijo12@gmail.com>
2024-11-14 16:58:03 -0500

    Terminado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,8 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="container mx-auto mt-10">
-      <h1 className="text-4xl font-bold mb-4">Bienvenido a Mi App MERN</h1>
-      <p className="text-xl">Inicia sesión o regístrate para comenzar a subir archivos.</p>
+      <h1 className="text-4xl font-bold mb-4">Bienvenido a MI STORAGE S3 AWS</h1>
+      <p className="text-xl">Inicia sesión o regístrate para comenzar a subir tus archivos.</p>
     </div>
   );
 };
